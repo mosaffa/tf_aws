2c7a0ef7316da67f181691d701464cbe5252eebd
mosaffa <mosaffa@gmail.com>
2020-05-12 00:18:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Manage simple AWS infrastructure with Terraform. Thanks Linux Academy
---
+++ variables.tf
@@ -0,0 +1,32 @@
+#----root/variables.tf-----
+variable "aws_region" {
+    
+}
+
+#------ storage variables
+variable "project_name" {
+
+}
+
+variable "access_key" {
+    
+}
+
+variable "secret_key" {
+  
+}
+
+#-------networking variables
+variable "vpc_cidr" {}
+variable "public_cidrs" {
+  type = "list"
+}
+variable "accessip" {}
+
+#-------compute variables
+variable "key_name" {}
+variable "public_key_mokia" {}
+variable "server_instance_type" {}
+variable "instance_count" {
+  default = 1
+}
